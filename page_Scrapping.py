8fb9dfa8fe8dcdbf809be2fbef1fa196fe30a5d0
Paulo Ubirajara de Mattos Neto <ubirajara3@hotmail.com>
2021-05-31 12:56:14 -0300

    Simplificar métodos

--- page_Scrapping.py
+++ page_Scrapping.py
@@ -1,9 +1,9 @@
 from typing import List
 
-import bs4
 from bs4 import BeautifulSoup
 import requests
-from smartphone_scrapping import Util
+
+from util import Util
 
 
 class Requisicoes:
@@ -12,30 +12,24 @@ class Requisicoes:
         self.db = self.create_smartphone_db(self.generate_page_links())
 
         self.util = Util(self.db)
+        print(self.util.getSmartphones())
 
     def get_phonelist_from_single_page(self, page_id: str) -> List[str]:
+        # Tentar otimizar as chamadas, ainda demora mt
         html = requests.get(page_id).content
         soup = BeautifulSoup(html, 'html.parser')
-        smartphone_ids = []
-        list_body = soup.find("div", id="cellphones_list")
-
-        for smartphone in list_body.children:
-            if type(smartphone) != bs4.element.NavigableString:
-                phone_pic = smartphone.find("a", class_="pic")
-                if phone_pic:
-                    smartphone_link = phone_pic.attrs["href"]
-                    if smartphone_link:
-                        smartphone_ids.append("https://www.tudocelular.com" + smartphone_link)
+        list_body = soup.find_all("a", class_="vedi_prezzi")
+        smartphone_ids = list(map(
+            lambda x: "https://www.tudocelular.com" + x.attrs['href'].replace('precos', 'fichas-tecnicas'), list_body))
 
         return smartphone_ids
 
     def generate_page_links(self) -> dict:
-        sequence = ["", "_2", "_3", "_4", "_5"]
+        available_pages = ["", "_2", "_3", "_4", "_5"]
 
-        # for i in sequence:
-        for i in range(3):
-            if i not in self.multiple_pages_links.keys():
-                current_link = f"https://www.tudocelular.com/celulares/fichas-tecnicas{sequence[i]}.html?o=1&ma=2500"
+        for i in available_pages:
+            if i not in self.multiple_pages_links:
+                current_link = f"https://www.tudocelular.com/celulares/fichas-tecnicas{i}.html?o=1&ma=2500"
                 self.multiple_pages_links[i] = current_link
 
         return self.multiple_pages_links
@@ -44,8 +38,6 @@ class Requisicoes:
         smartphone_list = []
 
         for j in link_list.values():
-            nested = self.get_phonelist_from_single_page(j)
-            for k in nested:
-                smartphone_list.append(k)
+            smartphone_list.extend(self.get_phonelist_from_single_page(j))
 
         return smartphone_list
